cf442e3af78b8b036a49dbe6ba2cc123a2b67ac4
Shriya Sorte <shriyasorte2000@gmail.com>
2024-06-26 00:51:05 +0530

    ticket controller code

--- controllers/ticket-controller.js
+++ controllers/ticket-controller.js
@@ -1,19 +1,71 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-function addticket(){
-
+function addticket() {
+  const {
+    tickettype, tstatus, ttitle, tdiscription, tattachment, tallocatedid, tremarks, tcreatedby, tmodifiedby,} = req.body;
+  console.log( tickettype, tstatus, ttitle, tdiscription, tattachment, tallocatedid, tremarks, tcreatedby, tmodifiedby);
+  const query = "INSERT INTO ticket (tickettype,tstatus,ttitle,tdiscription,tattachment,tallocatedid,tremarks,tcreatedby,tmodifiedby) VALUES (?,?,?,?,?,?,?,?,?)";
+  db.query(
+    query,
+    [tickettype,tstatus,ttitle,tdiscription,tattachment,tallocatedid,tremarks,tcreatedby,tmodifiedby,],(err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(result);
+    }
+  );
 }
 
-function getticket(){
-    
+function getticket() {
+  const query = "SELECT * FROM ticket";
+  db.query(query, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
 }
 
-function updateticket(){
+function updateticket() {
+  try {
+    const {tickettype, tstatus, ttitle, tdiscription, tattachment, tallocatedid, tremarks, tcreatedby, tmodifiedby, ticketid,} = req.body;
 
+    if (!ticketid) {
+      return res.status(400).send("Ticket ID is required for updating");
+    }
+
+    const query =
+      "UPDATE ticket SET tickettype =?, tstatus =?, ttitle =?, tdiscription =?, tattachment =?, tallocatedid =?, tremarks =?, tcreatedby =?, tmodifiedby =? WHERE ticketid =?";
+
+    db.query(
+      query,[ tickettype, tstatus, ttitle, tdiscription, tattachment, tallocatedid, tremarks, tcreatedby, tmodifiedby, ticketid,],(err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).send("An error occurred while updating the Ticket.");
+  }
 }
-function deleteticket(){
-
+function deleteticket() {
+  try {
+    const {tickettype,tstatus, ttitle, tdiscription, tattachment, tallocatedid, tremarks, tcreatedby, tmodifiedby, ticketid} = req.body;
+    if (!ticketid) {
+      return res.status(400).send("Ticket ID is required for deleting");
+    }
+    const query = "DELETE FROM user WHERE uid =?";
+    db.query(
+      query,
+      [ticketid, tickettype, tstatus, ttitle, tdiscription, tattachment, tallocatedid, tremarks, tcreatedby, tmodifiedby],(err, result)=>{
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.error("Error deleting Ticket:", error);
+    res.status(500).send("An error occurred while deleting the Ticket.");
+  }
 }
 
-
-module.exports = {addticket,getticket,updateticket,deleteticket};
+module.exports = { addticket, getticket, updateticket, deleteticket };
